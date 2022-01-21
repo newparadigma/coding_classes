45aff4bcbc974bed91d6855fb71a7f6c258b29b2
it_education <you@example.com>
2022-01-21 16:43:14 +0100

    wip

---
+++ data_to_xml.php
@@ -0,0 +1,125 @@
+<?php
+$zoos_general_info = [
+  331 => [
+    'owner' => 'Альберт Энштейн',
+    'city' => 'Москва'
+  ],
+  333 => [
+    'owner' => 'Джони Браво',
+    'city' => 'Москва'
+  ],
+  334 => [
+    'owner' => 'Джони Браво',
+    'city' => 'Волгоград'
+  ],
+  400 => [
+    'owner' => 'Лев Агутин',
+    'city' => 'Рязань'
+  ]
+];
+
+$zoos_work_hours = [
+  331 => [
+    'monday' => '09 - 16',
+    'tuesday' => '09 - 16',
+    'wednesday' => '',
+    'thursday' => '09 - 16',
+    'friday' => '09 - 20',
+    'saturday' => '09 - 14',
+    'sunday' => '',
+  ],
+  333 => [
+    'monday' => '10 - 15',
+    'tuesday' => '10 - 15',
+    'wednesday' => '10 - 15',
+    'thursday' => '10 - 15',
+    'friday' => '',
+    'saturday' => '10 - 15',
+    'sunday' => '',
+  ],
+  334 => [
+    'monday' => '10 - 15',
+    'tuesday' => '10 - 15',
+    'wednesday' => '10 - 15',
+    'thursday' => '10 - 15',
+    'friday' => '',
+    'saturday' => '10 - 15',
+    'sunday' => '',
+  ],
+  400 => [
+    'monday' => '',
+    'tuesday' => '10 - 18',
+    'wednesday' => '10 - 18',
+    'thursday' => '10 - 18',
+    'friday' => '10 - 18',
+    'saturday' => '',
+    'sunday' => '',
+  ]
+];
+
+$zoos_animals = [
+  331 => [
+    [
+      'superorder' => 'Жираф',
+      'males' => '5',
+      'females' => '5'
+    ],
+    [
+      'superorder' => 'Гепард',
+      'males' => '3',
+      'females' => '2'
+    ],
+    [
+      'superorder' => 'Хомяк',
+      'males' => '1',
+      'females' => '344'
+    ],
+    [
+      'superorder' => 'Лев',
+      'males' => '1',
+      'females' => ''
+    ],
+  ],
+  333 => [
+    [
+      'superorder' => 'Лев',
+      'males' => '5',
+      'females' => '20'
+    ],
+    [
+      'superorder' => 'Тигр',
+      'males' => '3',
+      'females' => '5'
+    ],
+  ],
+  334 => [
+    [
+      'superorder' => 'Лев',
+      'males' => '2',
+      'females' => '10'
+    ]
+  ],
+  400 => [
+    [
+      'superorder' => 'Бобр',
+      'males' => '4',
+      'females' => '7'
+    ],
+    [
+      'superorder' => 'Выдра',
+      'males' => '2',
+      'females' => '4'
+    ],
+    [
+      'superorder' => 'Жираф',
+      'males' => '',
+      'females' => '2'
+    ],
+    [
+      'superorder' => 'Лев',
+      'males' => '2',
+      'females' => '10'
+    ],
+  ]
+];
+ ?>
